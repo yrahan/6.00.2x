dd266edb8007d4fbec2255c9587c33c1082b2129
Yacine Bourkeb <yacine.bourkeb@gmail.com>
2014-03-09 17:55:54 +0100

    probleme 4 done

--- plotTemp.py
+++ plotTemp.py
@@ -1,6 +1,7 @@
 # The goal of this exercise is to produce a plot that shows the difference
 # between the high and the low temperature for each day.
-#import pylab
+import pylab
+from numpy import array
 
 
 def getMinMaxTemp(filename):
@@ -14,15 +15,26 @@ def getMinMaxTemp(filename):
             fields = line.split()
             if not(len(fields) != 3 or 'Boston' == fields[0] or
                    'Day' == fields[0]):
-                minTemps.append(int(fields[1]))
-                maxTemps.append(int(fields[2]))
+                minTemps.append(int(fields[2]))
+                maxTemps.append(int(fields[1]))
         return (minTemps, maxTemps)
+
+
+def producePlot(filename):
+    lowTemps, highTemps = getMinMaxTemp(filename)
+    pylab.figure(1)
+    diffTemps = array(highTemps) - array(lowTemps)
+    pylab.plot(range(1, 1 + diffTemps.size), diffTemps)
+    pylab.title('Day by Day Ranges in Temperature in Boston in July 2012')
+    pylab.xlabel('Days')
+    pylab.ylabel('Temperature Ranges')
+    pylab.savefig('figure-Temperature-Boston-July-2012')
+    #pylab.show()
 
 
 def main():
     filename = 'julyTemps.txt'
-    minTemps, maxTemps = getMinMaxTemp(filename)
-    print minTemps, maxTemps
+    producePlot(filename)
 
 if __name__ == "__main__":
         main()
