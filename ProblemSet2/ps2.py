08261c4fdc8550f0639e2790c7cd3e4acb431ccd
yrahan <yacine.bourkeb@gmail.com>
2014-03-31 07:53:42 +0200

    Problem3 done.

--- ProblemSet2/ps2.py
+++ ProblemSet2/ps2.py
@@ -268,10 +268,21 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
     robot_type: class of robot to be instantiated (e.g. StandardRobot or
                 RandomWalkRobot)
     """
-    raise NotImplementedError
+
+    room = RectangularRoom(width, height)
+    robots = []
+    for i in range(num_robots):
+        robots.append(robot_type(room, speed))
+    time_click = 1
+    while(room.getNumCleanedTiles() / float(room.getNumTiles()) < min_coverage):
+        for robot in robots:
+            robot.updatePositionAndClean()
+        time_click += 1
+    return float(time_click - 1)
+
 
 # Uncomment this line to see how much your simulation takes on average
-##print  runSimulation(1, 1.0, 10, 10, 0.75, 30, StandardRobot)
+print  runSimulation(1, 1.0, 10, 10, 0.75, 30, StandardRobot)
 
 
 # === Problem 4
