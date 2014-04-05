4b88bd5407233ba4f6237876b97fe252b1fdfb9f
yrahan <yacine.bourkeb@gmail.com>
2014-04-05 07:45:19 +0200

    week 3 done.

--- histogramfun.py
+++ histogramfun.py
@@ -1,12 +1,24 @@
 import pylab
 
+
+#set line width
+pylab.rcParams['lines.linewidth'] = 6
+#set font size for titles
+pylab.rcParams['axes.titlesize'] = 20
+#set font size for labels on axes
+pylab.rcParams['axes.labelsize'] = 20
+#set size of numbers on x-axis
+pylab.rcParams['xtick.major.size'] = 5
+#set size of numbers on y-axis
+pylab.rcParams['ytick.major.size'] = 5
 # You may have to change this path
 WORDLIST_FILENAME = "words.txt"
+
 
 def loadWords():
     """
     Returns a list of valid words. Words are strings of uppercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     """
@@ -20,12 +32,29 @@ def loadWords():
     print "  ", len(wordList), "words loaded."
     return wordList
 
+
 def plotVowelProportionHistogram(wordList, numBins=15):
     """
     Plots a histogram of the proportion of vowels in each word in wordList
     using the specified number of bins in numBins
     """
-    
+
+    vals = []
+    vowels = 'aeiou'
+    for w in wordList:
+        vals.append(sum({vowel: w.count(vowel) for vowel in vowels}.values())
+                    / float(len(w)))
+    pylab.hist(vals, numBins)
+    xmin, xmax = pylab.xlim()
+    ymin, ymax = pylab.ylim()
+    print 'x-range =', xmin, '-', xmax
+    print 'y-range =', ymin, '-', ymax
+    pylab.figure
+    ##pylab.hist(vals, bins = 11)
+    ###pylab.xlim(-1.0, 2.0)
+    pylab.show()
+    ##assert False
+
 
 if __name__ == '__main__':
     wordList = loadWords()
