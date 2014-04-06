eee80ddc6f13c1613d7cbd8ef0235d314e94e1b6
yrahan <yacine.bourkeb@gmail.com>
2014-04-06 18:27:10 +0200

    done PROBLEM 5-1

---
+++ quizz.py
@@ -0,0 +1,15 @@
+import random
+
+
+def sampleQuizzes():
+    # Your code here
+    numTrials = 100000
+    yes = 0
+    for trial in range(numTrials):
+        midterm1 = random.randrange(50, 81)
+        midterm2 = random.randrange(60, 91)
+        finalExam = random.randrange(55, 96)
+        grade = 0.25 * (midterm1 + midterm2) + 0.50 * finalExam
+        if grade >= 70 and grade <= 75:
+            yes += 1
+    return yes / float(numTrials)
