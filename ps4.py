37ca18c0115cddee230567038fd290e3f97378ea
yrahan <yacine.bourkeb@gmail.com>
2014-04-15 23:45:41 +0200

    done.

--- ps4.py
+++ ps4.py
@@ -7,7 +7,7 @@ from ps3b import *
 
 #
 # PROBLEM 1
-#        
+#
 def simulationDelayedTreatment(numTrials):
     """
     Runs simulations and make histograms for problem 1.
@@ -21,7 +21,53 @@ def simulationDelayedTreatment(numTrials):
 
     numTrials: number of simulation runs to execute (an integer)
     """
-    
+
+    # TreatedPatient parameters
+    numViruses = 100
+    maxPop = 1000
+    # ResistantVirus parameters
+    maxBirthProb = 0.1
+    clearProb = 0.05
+    resistances = {'guttagonol': False}
+    mutProb = 0.005
+
+    # average number of viruses given by simulations
+    delay = 75
+    treatementTime = 150
+    times = range(delay + treatementTime)
+    drug = 'guttagonol'
+    viruses_final = [0] * numTrials
+    ##viruses_sum = [0] * numTrials
+    ##viruses_resistant_sum = [0] * numTrials
+    for trial in range(numTrials):
+        viruses = [ResistantVirus(maxBirthProb, clearProb,
+                                  resistances, mutProb)
+                   for v in range(numViruses)]
+        patient = TreatedPatient(viruses, maxPop)
+        for time in times:
+            if time == delay:
+                patient.addPrescription(drug)
+            viruses_final[trial] = patient.update()
+            ##viruses_sum[time] += patient.update()
+            ##viruses_resistant_sum[time] += patient.getResistPop([drug])
+
+    pylab.hist(viruses_final)
+    ##pylab.hist(viruses_final, bins=20)
+    pylab.show()
+
+    ##viruses_avg = [vsum / float(numTrials) for vsum in viruses_sum]
+    ##viruses_resistant_avg = [vsum / float(numTrials)
+    ##                         for vsum in viruses_resistant_sum]
+    # print viruses_avg
+    # print viruses_resistant_avg
+    # plotting
+    ## pylab.plot(times, viruses_avg, label="Total")
+    ##pylab.plot(times, viruses_resistant_avg, label="Resistant viruses")
+    ##pylab.title("ResistantVirus simulation")
+    ##pylab.legend("Average number of viruses")
+    ##pylab.xlabel("time step")
+    ##pylab.ylabel("# viruses")
+    ##pylab.show()
     # TODO
 
 
